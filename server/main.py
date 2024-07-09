4d2995356fdfb4c6721b648f72daea5dbe49b7ee
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-07-09 13:54:26 +0100

    add env to our main.py

--- server/main.py
+++ server/main.py
@@ -170,4 +170,5 @@ def get_research_grants():
 
  
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
